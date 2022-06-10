5c5a52a377b7a15011cbfc1cbe0c4d51a29d7436
salineselin <salineselin@gmail.com>
2022-06-10 12:28:56 -0600

    added image registry

---
+++ pulumi/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/container"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		_, err := container.NewRegistry(ctx, "registry", &container.RegistryArgs{
+			Location: pulumi.String("US"),
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
