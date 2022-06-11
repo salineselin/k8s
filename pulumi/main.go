ca7107e16eaf45ae3accb96dda6fe43c8d06c9d5
salineselin <salineselin@gmail.com>
2022-06-10 23:28:00 -0600

    switched from gcr to artifact registry

--- pulumi/main.go
+++ pulumi/main.go
@@ -1,26 +1,33 @@
 package main
 
 import (
-	"strings"
-
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/container"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp"
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/artifactregistry"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/serviceaccount"
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// create a container registry
-		reg, err := container.NewRegistry(ctx, "registry", &container.RegistryArgs{
-			Location: pulumi.String("US"),
-		})
+		// add google beta provider
+		google_beta, err := gcp.NewProvider(ctx, "google-beta", &gcp.ProviderArgs{})
+		if err != nil {
+			return err
+		}
+
+		// create an artifact registry
+		reg, err := artifactregistry.NewRepository(ctx, "saline-selin-artifacts-repo", &artifactregistry.RepositoryArgs{
+			Location:     pulumi.String("us-west3-c"),
+			RepositoryId: pulumi.String("salinesel.in"),
+			Description:  pulumi.String("example docker repository"),
+			Format:       pulumi.String("DOCKER"),
+		}, pulumi.Provider(google_beta))
 		if err != nil {
 			return err
 		}
 
 		// Create an admin serviceaccount
-		saName := "gcr-admin"
+		saName := "saline-selin-artifacts-admin"
 		sa, err := serviceaccount.NewAccount(ctx, saName, &serviceaccount.AccountArgs{
 			AccountId:   pulumi.String(saName),
 			DisplayName: pulumi.String(saName),
@@ -29,12 +36,10 @@ func main() {
 			return err
 		}
 
-		// bind it to the container registry
-		_, err = storage.NewBucketIAMMember(ctx, "give-sa-bucket-permissions", &storage.BucketIAMMemberArgs{
-			Role: pulumi.String("roles/storage.admin"),
-			Bucket: reg.BucketSelfLink.ApplyT(func(link string) string {
-				return strings.TrimLeft(link, "/")
-			}).(pulumi.StringOutput),
+		// bind it to the artifact registry
+		_, err = artifactregistry.NewRepositoryIamMember(ctx, "saline-selin-artifacts-binding", &artifactregistry.RepositoryIamMemberArgs{
+			Repository: reg.Name,
+			Role:       pulumi.String("roles/artifactregistry.admin"),
 			Member: sa.Email.ApplyT(func(Email string) string {
 				return "serviceAccount:" + Email
 			}).(pulumi.StringOutput),
